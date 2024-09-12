569841621e58ece1a4ad3508bf72156e305fcd21
Siyeong Choi <siychoi@c2r1s4.42seoul.kr>
2024-09-12 18:28:50 +0900

    add: channel functions

--- Channel.hpp
+++ Channel.hpp
@@ -8,6 +8,21 @@
 
 class Channel {
     public:
+        void    kick(Client* client);
+        void    invite(Client* client);
+        void    topic(Client* client, const std::string& topic);
+
+        void    set_option_i();
+        void    set_option_t();
+        void    set_option_k(std::string key);
+        void    set_option_o(Client* client);
+        void    set_option_l(unsigned int users_limit);
+
+        void    unset_option_i();
+        void    unset_option_t();
+        void    unset_option_k();
+        void    unset_option_o(Client* client);
+        void    unset_option_l();
 
     private:
         std::vector<Client*> clients;
@@ -17,9 +32,11 @@ class Channel {
         std::string channel_topic;
         std::string channel_key; // password
 
-        // int channel_users_limit;
-        // bool is_topic_restrict;
-        // bool is_invite_only;
+        bool is_invite_only;
+        bool is_topic_restrict;
+        std::string channel_key;
+        unsigned int channel_users_limit;
+
         
 };
 
