1a75e26d385b32fc8d36da989ecd766f49dcbbf4
Youngho Cho <dominoyh5@gmail.com>
2024-09-15 14:45:49 +0900

    impl: Error::*() for NICK, USER, PRIVMSG

--- Error.cpp
+++ Error.cpp
@@ -1,7 +1,37 @@
 #include "Error.hpp"
 
-// <client> <command> :Unknown command
-// :lead.libera.chat 421 younghoc ASDF :Unknown command
-std::string Error::err_unknowncommand(const std::string &nickname, const std::string &command) {
-    return "421 " + nickname + " " + command + " :Unknown command";
+std::string Error::err_unknowncommand(const std::string &nickname, const std::string &wrong_command) {
+    return "421 " + nickname + " " + wrong_command + " :Unknown command";
+}
+
+std::string Error::err_nosuchnick(const std::string &nickname, const std::string &wrong_recipient) {
+    return "401 " + nickname + " " + wrong_recipient + " :No such nick/channel";"
+}
+
+std::string Error::err_norecipient(const std::string &nickname, const std::string &command) {
+    return "411 " + nickname + " :No recipient given (" + command + ")";
+}
+
+std::string Error::err_notexttosend(const std::string &nickname) {
+    return "412 " + nickname + " :No text to send";
+}
+
+std::string Error::err_nonicknamegiven() {
+    return "431 * :No nickname given";
+}
+
+std::string Error::err_erroneusnickname(const std::string &wrong_nickname) {
+    return "432 * " + wrong_nickname + " :Erroneus nickname";
+}
+
+std::string Error::err_nicknameinuse(const std::string &wrong_nickname) {
+    return "433 * " + wrong_nickname + " :";
+}
+
+std::string Error::err_needmoreparams(const std::string &command) {
+    return "461 * " + command + " :Not enough parameters";
+}
+
+std::string Error::err_alreadyregistered(const std::string &nickname) {
+    return "462 " + nickname + " :You may not reregister";
 }
--- Error.hpp
+++ Error.hpp
@@ -7,34 +7,34 @@ class Error {
     public:
     // <client> <command> :Unknown command
     // :lead.libera.chat 421 younghoc ASDF :Unknown command
-    static std::string err_unknowncommand(const std::string &nickname, const std::string &command);    // 421
+    static std::string err_unknowncommand(const std::string &nickname, const std::string &wrong_command);    // 421
 
     // <client> <nickname> :No such nick/channel
     // :silver.libera.chat 401 younghoc asdfawefawe :No such nick/channel
-    static std::string err_nosuchnick();    // 401
+    static std::string err_nosuchnick(const std::string &nickname, const std::string &wrong_recipient);    // 401
     // <client> :No recipient given (<command>)
     // :silver.libera.chat 411 younghoc :No recipient given (PRIVMSG)
-    static std::string err_norecipient();   // 411
+    static std::string err_norecipient(const std::string &nickname, const std::string &command);   // 411
     // <client> :No text to send
     // :silver.libera.chat 412 younghoc :No text to send
-    static std::string err_notexttosend();  // 412
+    static std::string err_notexttosend(const std::string &nickname);  // 412
 
     // <client> :No nickname given
     // :tantalum.libera.chat 431 * :No nickname given
     static std::string err_nonicknamegiven();   // 431
     // <client> <nick> :Erroneus nickname
     // :tantalum.libera.chat 432 * awef@#$ :Erroneous Nickname
-    static std::string err_erroneusnickname();  // 432
+    static std::string err_erroneusnickname(const std::string &wrong_nickname);  // 432
     // <client> <nick> :Nickname is already in use
     // :tantalum.libera.chat 433 * asdf :Nickname is already in use.
-    static std::string err_nicknameinuse();     // 433
+    static std::string err_nicknameinuse(const std::string &wrong_nickname);     // 433
 
     // <client> <command> :Not enough parameters
     // :iridium.libera.chat 461 * USER :Not enough parameters
-    static std::string err_needmoreparams();    // 461
+    static std::string err_needmoreparams(const std::string &command);    // 461
     // <client> :You may not reregister
     // :calcium.libera.chat 462 younghoc :You are already connected and cannot handshake again
-    static std::string err_alreadyregistered(); // 462
+    static std::string err_alreadyregistered(const std::string &nickname); // 462
 
     private:
 };
