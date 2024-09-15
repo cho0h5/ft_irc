5766999e6919ac920faabdd09c52bf8f912a0ab1
Youngho Cho <dominoyh5@gmail.com>
2024-09-15 15:15:53 +0900

    apply: Error::*()

--- Command_hyowchoi.cpp
+++ Command_hyowchoi.cpp
@@ -1,3 +1,4 @@
+#include "Error.hpp"
 #include "Server.hpp"
 #include "Client.hpp"
 // #include <sstream>
@@ -40,7 +41,6 @@
 // NICK <new_nickname>
 // TODO : client 객체 생성 시 nickname 임의로 지정
 void Server::command_nick(const int fd, const std::vector<std::string> &cmds) {
-
     // not NICK command
     if (cmds[0] != "NICK") {
         return;
@@ -48,13 +48,13 @@ void Server::command_nick(const int fd, const std::vector<std::string> &cmds) {
 
     // no new_nickname : ERR_NONICKNAMEGIVEN, 431
     if (cmds.size() != 2) {
-        send_error(fd, 431);
+        clients_fd[fd].send_message(Error::err_nonicknamegiven());
         return;
     }
 
     // already registered nickname : ERR_NICKNAMEINUSE, 433
     if (clients_nickname.find(cmds[1]) != clients_nickname.end()) {
-        send_error(fd, 433);
+        clients_fd[fd].send_message(Error::err_nicknameinuse(cmds[1]));
         return;
     }
 
@@ -62,12 +62,12 @@ void Server::command_nick(const int fd, const std::vector<std::string> &cmds) {
     for (unsigned long i = 0; i < cmds[1].size(); i++) {
         const char c = cmds[1][i];
         if (!(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9'))) {
-            send_error(fd, 432);
+            clients_fd[fd].send_message(Error::err_erroneusnickname(cmds[1]));
             return;
         }
     }
     if (('0' <= cmds[1][0] && cmds[1][0] <= '9')) {
-        send_error(fd, 432);
+        clients_fd[fd].send_message(Error::err_erroneusnickname(cmds[1]));
         return;
     }
     // change nickname
@@ -91,20 +91,21 @@ void Server::command_user(const int fd, const std::vector<std::string> &cmds) {
 
     // no username : ERR_NEEDMOREPARAMS, 461
     if (cmds.size() != 5 || cmds[1].empty() || cmds[4].empty()) {
-        send_error(fd, 461);
+        clients_fd[fd].send_message(Error::err_needmoreparams(cmds[0]));
         return;
     }
 
     // already registered username : ERR_ALREADYREGISTRED, 462
     std::map<int, Client>::iterator it = clients_fd.find(fd);
     if (it->second.get_username() != "") {
-        send_error(fd, 462);
+        const std::string &nickname = clients_fd[fd].get_nickname();
+        clients_fd[fd].send_message(Error::err_alreadyregistered(nickname));
         return;
     }
 
     // username contains space, but not prefixed with colon : 무슨 에러징...
     if (cmds[5].find(" ") != std::string::npos && cmds[5][0] != ':') {
-        send_error(fd, 461);
+        clients_fd[fd].send_message(Error::err_needmoreparams(cmds[0]));
         return;
     }
     // change username
@@ -117,13 +118,16 @@ void Server::command_user(const int fd, const std::vector<std::string> &cmds) {
 }
 
 void Server::command_privmsg_user(const int fd, const std::vector<std::string> &cmds) {
+    const std::string command = cmds[0];
+    const std::string &nickname = clients_fd[fd].get_nickname();
+
     if (cmds.size() == 1) {
-        send_error(fd, 411);
+        clients_fd[fd].send_message(Error::err_norecipient(nickname, command));
         return;
     }
 
     if (cmds.size() == 2) {
-        send_error(fd, 412);
+        clients_fd[fd].send_message(Error::err_notexttosend(nickname));
         return;
     }
 
@@ -132,12 +136,11 @@ void Server::command_privmsg_user(const int fd, const std::vector<std::string> &
 
     std::map<std::string, Client*>::iterator it = clients_nickname.find(recipient);
     if (it == clients_nickname.end()) {
-        send_error(fd, 401);
+        clients_fd[fd].send_message(Error::err_nosuchnick(nickname, recipient));
         return;
     }
 
     Client &client = clients_fd[fd];
-    const std::string nickname = client.get_nickname();
     it->second->send_message(":" + client.get_identifier() + " PRIVMSG " + nickname + " :" + message);
 }
 
